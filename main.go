31a2f83f832c0f10249bbf95f555123e8a53f955
Tomas-Ka <tomas.kamsater@gmail.com>
2025-05-10 16:16:53 +0200

    Add some more base pages and routes

--- main.go
+++ main.go
@@ -13,11 +13,10 @@ var t = template.Must(template.ParseFS(views, "views/*"))
 
 func main() {
 	router := http.NewServeMux()
-	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		if err := t.ExecuteTemplate(w, "index.html", nil); err != nil {
-			http.Error(w, "Something went wrong", http.StatusInternalServerError)
-		}
-	})
+	router.HandleFunc("/index", index)
+	router.HandleFunc("/admin", admin)
+	router.HandleFunc("/sign-up", signup)
+	router.HandleFunc("/leaderboard", leaderboard)
 
 	server := &http.Server{
 		Addr:    ":8080",
@@ -26,4 +25,30 @@ func main() {
 
 	fmt.Println("Server is running at localhost:8080")
 	_ = server.ListenAndServe()
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	if err := t.ExecuteTemplate(w, "index.html", nil); err != nil {
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+	}
+	if 0 == 1 {
+		if err := t.ExecuteTemplate(w, "userpage.html", nil); err != nil {
+			http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		}
+	}
+}
+func admin(w http.ResponseWriter, r *http.Request) {
+	if err := t.ExecuteTemplate(w, "admin.html", nil); err != nil {
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+	}
+}
+func signup(w http.ResponseWriter, r *http.Request) {
+	if err := t.ExecuteTemplate(w, "signup.html", nil); err != nil {
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+	}
+}
+func leaderboard(w http.ResponseWriter, r *http.Request) {
+	if err := t.ExecuteTemplate(w, "leaderboard.html", nil); err != nil {
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+	}
 }
