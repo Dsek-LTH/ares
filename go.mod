d66b16e49f4a7f7c206aabaffa2990463dbe2cce
Esbjörn Stenberg <me@stagrim.com>
2025-05-11 00:31:06 +0200

    Added simple OpenID authentication

--- go.mod
+++ go.mod
@@ -9,8 +9,20 @@ require (
 )
 
 require (
+	github.com/coreos/go-oidc/v3 v3.14.1 // indirect
+	github.com/go-jose/go-jose/v4 v4.0.5 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
+	github.com/gorilla/sessions v1.4.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-sqlite3 v1.14.28 // indirect
+	github.com/zitadel/oidc v1.13.5 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/oauth2 v0.30.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
-	// "github.com/a-h/templ"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
+	"log"
+	"os"
+
 	"killer-game/components"
 	"net/http"
+
+	"github.com/coreos/go-oidc/v3/oidc"
+	"github.com/gorilla/sessions"
+	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var (
+	sessionStore *sessions.CookieStore
 )
 
 type User struct {
@@ -46,9 +58,30 @@ type leaderboardHandler struct {
 type adminHandler struct {
 }
 
-func (ih indexHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	ih.username = "aaa"
-	components.Index(ih.username).Render(r.Context(), w)
+func authMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		session, _ := sessionStore.Get(r, "auth-session")
+		_, ok := session.Values["username"]
+
+		if !ok {
+			// Not logged in
+			http.Redirect(w, r, "/login", http.StatusFound)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	session, _ := sessionStore.Get(r, "auth-session")
+	username, ok := session.Values["username"].(string)
+	log.Println(session.Values)
+	if ok {
+		components.Index(username).Render(r.Context(), w)
+	} else {
+		components.Index("Not Logged In").Render(r.Context(), w)
+	}
 }
 
 func (sh signUpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -64,9 +97,39 @@ func (lh leaderboardHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	clientID := os.Getenv("CLIENT_ID")
+	clientSecret := os.Getenv("CLIENT_SECRET")
+	redirectURL := os.Getenv("REDIRECT_URL")
+	issuer := os.Getenv("ISSUER")
+
+	sessionStore = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
+
+	ctx := context.Background()
+
+	provider, err := oidc.NewProvider(ctx, issuer)
+	if err != nil {
+		log.Fatalf("Failed to discover provider: %v", err)
+	}
+
+	verifier := provider.Verifier(&oidc.Config{
+		ClientID: clientID,
+	})
+	oauth2Config := &oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		RedirectURL:  redirectURL,
+		Endpoint:     provider.Endpoint(),
+		Scopes:       []string{oidc.ScopeOpenID, "profile"},
+	}
+
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
-		panic("failed to connect database")
+		log.Fatalf("failed to connect database")
 	}
 
 	// Migrate the schema
@@ -76,17 +139,75 @@ func main() {
 
 	// Routes
 	router := http.NewServeMux()
-	router.Handle("/{$}", indexHandler{})
+	router.HandleFunc("/{$}", IndexHandler)
 	router.Handle("/admin", adminHandler{})
 	router.Handle("GET /sign-up", adminHandler{})
 	router.Handle("PUT /sign-up", signUpHandler{})
 	router.Handle("/leaderboard", leaderboardHandler{})
+
+	router.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, oauth2Config.AuthCodeURL("some-random-state"), http.StatusFound)
+	})
+
+	router.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		session, _ := sessionStore.Get(r, "auth-session")
+		session.Options.MaxAge = -1
+		session.Save(r, w)
+		http.Redirect(w, r, "/", http.StatusFound)
+	})
+
+	router.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+
+		if r.URL.Query().Get("state") != "some-random-state" {
+			http.Error(w, "State mismatch", http.StatusBadRequest)
+			return
+		}
+
+		oauth2Token, err := oauth2Config.Exchange(ctx, r.URL.Query().Get("code"))
+		if err != nil {
+			http.Error(w, "Failed to exchange token", http.StatusInternalServerError)
+			return
+		}
+
+		// Extract the ID Token from OAuth2 token
+		rawIDToken, ok := oauth2Token.Extra("id_token").(string)
+		if !ok {
+			http.Error(w, "No id_token field in oauth2 token", http.StatusInternalServerError)
+			return
+		}
+
+		// Verify ID Token
+		idToken, err := verifier.Verify(ctx, rawIDToken)
+		if err != nil {
+			http.Error(w, "Failed to verify ID token", http.StatusInternalServerError)
+			return
+		}
+
+		// Extract claims
+		var claims map[string]interface{}
+		if err := idToken.Claims(&claims); err != nil {
+			http.Error(w, "Failed to parse claims", http.StatusInternalServerError)
+			return
+		}
+
+		// Save to session
+		session, _ := sessionStore.Get(r, "auth-session")
+		log.Println(claims["preferred_username"])
+		session.Values["username"] = claims["preferred_username"]
+		err = session.Save(r, w)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Session store error: %s", err.Error()), http.StatusInternalServerError)
+		}
+
+		http.Redirect(w, r, "/", http.StatusFound)
+	})
 
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: router,
 	}
 
-	fmt.Println("Server is running at localhost:8080")
-	_ = server.ListenAndServe()
+	log.Printf("Server is running at localhost:8080")
+	log.Fatal(server.ListenAndServe())
 }
